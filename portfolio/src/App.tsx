d8ce8af7ead90276bedc70846ec2781a61b926b8
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2024-03-01 21:17:10 +0600

    added myself

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Physics } from '@react-three/rapier'
-import { Billboard, Environment, Html, Image, KeyboardControls, Loader, Sphere } from '@react-three/drei'
+import { AsciiRenderer, Billboard, Environment, Html, Image, KeyboardControls, Loader, Sphere, Text } from '@react-three/drei'
 import { Suspense, useEffect, useState } from 'react'
 import { EcctrlJoystick } from 'ecctrl'
 import { getProject } from '@theatre/core'
@@ -10,6 +10,7 @@ import { SheetProvider } from '@theatre/r3f'
 import demoProjectState from './state.json'
 import { editable as e } from '@theatre/r3f'
 import nyan from "./assets/nyan.png"
+import strat from "./assets/strat.png"
 
 // our Theatre.js project sheet, we'll use this later
 const demoSheet = getProject('Demo Project', { state: demoProjectState }).sheet('Demo Sheet')
@@ -74,9 +75,9 @@ export default function App() {
           far: 1000,
         }}
         onPointerDown={(e) => {
-          // if (e.pointerType === 'mouse') {
-          //   (e.target as HTMLCanvasElement).requestPointerLock()
-          // }
+          if (e.pointerType === 'mouse') {
+            (e.target as HTMLCanvasElement).requestPointerLock()
+          }
         }}>
         {/* <ContactShadows position={[0, -0.2, 0]} far={1000} /> */}
         <SheetProvider sheet={demoSheet}>
@@ -108,20 +109,23 @@ export default function App() {
             </KeyboardControls>
             <Ground />
           </Physics>
-          <e.group theatreKey='htmltext'>
-            {/* <mesh>
+          {/* <mesh>
               <Html position={[0, -0.8, 10]} transform occlude="raycast">
                 <div>
                   <img src='https://media.tenor.com/5Z5h-ffbqj0AAAAj/%D0%BA%D0%BE%D1%82%D1%83%D1%81%D0%BB%D0%B5%D1%82%D0%BE%D1%83%D1%81.gif' className="w-64 h-64 object-cover" />
                 </div>
               </Html>
             </mesh> */}
-            <Billboard>
+          <Billboard>
+            <e.group theatreKey='nyancat'>
               <Image url={nyan} transparent />
-            </Billboard>
-          </e.group>
+            </e.group>
+            <e.group theatreKey='strat'>
+              <Image url={strat} transparent zoom={0.35} />
+            </e.group>
+          </Billboard>
         </SheetProvider>
-      </Canvas>
+      </Canvas >
       <Loader />
     </>
   )
